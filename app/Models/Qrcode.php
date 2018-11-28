4e5923567d6cbfeaf5413cb5eb7e4acc46bbc510
ken <kennedywanjahi@gmail.com>
2018-11-28 11:53:24 +0300

    generating files using laravel generator

---
+++ app/Models/Qrcode.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Qrcode
+ * @package App\Models
+ * @version November 28, 2018, 8:43 am UTC
+ *
+ * @property integer user_id
+ * @property string website
+ * @property string product_name
+ * @property string product_url
+ * @property string company_name
+ * @property string callback_url
+ * @property string qrcode_path
+ * @property float amount
+ * @property boolean status
+ */
+class Qrcode extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'qrcodes';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'user_id',
+        'website',
+        'product_name',
+        'product_url',
+        'company_name',
+        'callback_url',
+        'qrcode_path',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'website' => 'string',
+        'product_name' => 'string',
+        'product_url' => 'string',
+        'company_name' => 'string',
+        'callback_url' => 'string',
+        'qrcode_path' => 'string',
+        'amount' => 'float',
+        'status' => 'boolean'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
