8dbe1d202e3a59e6726a62f7f06e7a9f5ee0cbec
ken <kennedywanjahi@gmail.com>
2018-12-06 17:32:49 +0300

    roles clean up and tweaks

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -2,16 +2,23 @@
 
 @section('content')
     <section class="content-header">
-        <h1>
-            Role
+        <h1 class="pull-left">
+            User Roles
         </h1>
+        <h1 class="pull-right">
+        <a href="{!! route('roles.edit', [$role->id]) !!}" class='btn btn-primary'>
+          Edit User Role
+          <i class="glyphicon glyphicon-edit"></i></a>
+          </h1>
     </section>
     <div class="content">
+      <div class="clearfix">
+
+      </div>
         <div class="box box-primary">
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('roles.show_fields')
-                    <a href="{!! route('roles.index') !!}" class="btn btn-default">Back</a>
                 </div>
             </div>
         </div>
