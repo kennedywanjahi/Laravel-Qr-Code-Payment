45d770b5f4e5e47c75f2f61c52379ffbb27c8d21
ken <kennedywanjahi@gmail.com>
2018-12-06 17:49:14 +0300

    clean up roles index

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -12,9 +12,11 @@
           </h1>
     </section>
     <div class="content">
-      <div class="clearfix">
+      <div class="clearfix"></div>
 
-      </div>
+      @include('flash::message')
+
+      <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
